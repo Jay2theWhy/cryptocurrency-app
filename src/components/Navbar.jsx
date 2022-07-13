8788f230f6af061ea6b8db289cc758fd6f9ae4a7
Jing Yang Fan <jyfan16@hotmail.com>
2022-07-13 17:53:17 -0400

    got the navbar component working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,15 @@ import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
+import icon from '../images/cryptocurrency.png';
+
 const Navbar = () => {
   return (
     <div className="nav-container">
         <div className="logo-container">
-            <Avatar />
+            <Avatar src={icon} size="large" />
             <Typography.Title level={2} className="logo">
-                <Link to="/">Cryptocurrency-app</Link>
+                <Link to="/">Cryptocurrency App</Link>
             </Typography.Title>
             {/* <Button className="menu-control-container">
 
