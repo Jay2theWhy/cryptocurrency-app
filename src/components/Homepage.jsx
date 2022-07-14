207475bd0b7a14e55512923a44e7cc033819c04e
Jing Yang Fan <jyfan16@hotmail.com>
2022-07-14 16:46:20 -0400

    added cryptocurrency and news section to homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,7 +3,11 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
+// import store
 import { useGetCryptosQuery } from '../services/cryptoApi';
+
+// import components
+import { Cryptocurrencies, News } from '../components';
 
 const { Title } = Typography;
 
@@ -25,6 +29,16 @@ const Homepage = () => {
         <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
         <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
+        <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Latest Crypto News</Title>
+        <Title level={3} className="show-more"><Link to="/news">Show More</Link></Title>
+      </div>
+      <News simplified />
     </>
   )
 }
