0a72b01d2ba974313b78eba04734f1968c39802c
Jing Yang Fan <jyfan16@hotmail.com>
2022-07-14 01:27:50 -0400

    hid api key

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router} from 'react-router-dom';
-import { Provide } from 'react-redux';
+import { Provider } from 'react-redux';
 
 import App from './App';
 import store from './app/store';
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
 const cryptoApiHeaders = {
-    'X-RapidAPI-Key': '133ea77deemsh8258430ed30e8d9p14cb0fjsna9cb654234a9',
+    'X-RapidAPI-Key': process.env.RAPID_API_KEY,
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
 }
 
@@ -13,7 +13,7 @@ export const cryptoApi = createApi({
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
         getCryptos: builder.query({
-            query: () => createRequest('/exchanges')
+            query: () => createRequest('/coins')
         })
     })
 })
