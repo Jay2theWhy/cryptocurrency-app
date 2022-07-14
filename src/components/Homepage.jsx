692e5815b53fdb2341d0a926bbc00e6bb7417719
Jing Yang Fan <jyfan16@hotmail.com>
2022-07-14 01:17:34 -0400

    implement redux boiler plate

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 
 const { Title } = Typography;
 
+
 const Homepage = () => {
   return (
     <>
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,17 @@
+
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       limit: '50',
+//       offset: '0',
+//       orderBy: '24hVolume',
+//       orderDirection: 'desc'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': '133ea77deemsh8258430ed30e8d9p14cb0fjsna9cb654234a9',
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+//   };
