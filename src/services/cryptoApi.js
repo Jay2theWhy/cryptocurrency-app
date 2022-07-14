0b21c0553cb52a2782814acc006e18d32419228c
Jing Yang Fan <jyfan16@hotmail.com>
2022-07-14 01:25:27 -0400

    added api connection

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,17 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '133ea77deemsh8258430ed30e8d9p14cb0fjsna9cb654234a9',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
 
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
-// const options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
-//     params: {
-//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//       limit: '50',
-//       offset: '0',
-//       orderBy: '24hVolume',
-//       orderDirection: 'desc'
-//     },
-//     headers: {
-//       'X-RapidAPI-Key': '133ea77deemsh8258430ed30e8d9p14cb0fjsna9cb654234a9',
-//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-//     }
-//   };
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/exchanges')
+        })
+    })
+})
