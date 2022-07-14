e2fc4b6c91bc7c25c67defcf835a849d5c3032f1
Jing Yang Fan <jyfan16@hotmail.com>
2022-07-13 23:30:04 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -23,9 +23,17 @@ const App = () => {
                     </Routes>
                 </div>
             </Layout>
-        </div>
-        <div className="footer">
-
+            <div className="footer">
+                <Typography.Title level={5} style={{ color: "white", textAlign: "center"}}>
+                    Cryptocurrency App <br />
+                    All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                    <Link to="/news">News</Link>
+                </Space>
+            </div>
         </div>
     </div>
   )
