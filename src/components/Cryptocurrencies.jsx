076f15d4634cd19eab4acb350f44ab8e5f314cdc
Jing Yang Fan <jyfan16@hotmail.com>
2022-07-15 00:46:28 -0400

    generated a card for each cryptocurrency

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -13,7 +13,25 @@ const Cryptocurrencies = () => {
   console.log(cryptos)
 
   return (
-    <div>Cryptocurrencies</div>
+    <>
+      <Row gutter={[12, 12]} className="crypto-card-container">
+        {cryptos.map((currency) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+            <Link to={`/crypto/${currency.id}`}>
+              <Card 
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {millify(currency.change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
 
